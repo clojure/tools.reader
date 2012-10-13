e83584d28ffb4a48a35b818fbd642675d4499b74
Bronsa <brobronsa@gmail.com>
2012-10-13 14:21:24 +0200

    init

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject bronsa/blind "0.1.0-SNAPSHOT"
+  :description "A clojure reader written in clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
