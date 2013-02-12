47bd9ed9c90f1a2f2b2b48418efd47185c7f35b4
Bronsa <brobronsa@gmail.com>
2013-02-13 00:12:42 +0100

    updated project.clj

--- project.clj
+++ project.clj
@@ -6,4 +6,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.5.0-RC4"]])
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5   {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
+  :min-lein-version "2.0.0")
