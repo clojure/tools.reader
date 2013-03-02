5f3be329d1b020d9152d6ab5fbc906b3ecdfdb0c
Bronsa <brobronsa@gmail.com>
2013-03-02 14:05:41 +0100

    updated project.clj for clojure 1.6

--- project.clj
+++ project.clj
@@ -8,9 +8,10 @@
   :test-paths ["src/test/clojure"]
   :aot [clojure.tools.reader.impl.ExceptionInfo]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5   {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
-  :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5:test,1.6" "test"]}
   :min-lein-version "2.0.0")
