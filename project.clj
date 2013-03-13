f8c7a7b0aed6ba5084768a6ef29e869ab0a753ad
Bronsa <brobronsa@gmail.com>
2013-03-13 15:04:29 +0100

    use 1.5.1 in project.clj

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
   :dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
   :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5:test,1.6" "test"]}
   :min-lein-version "2.0.0")
