273b8ae83a0b8ce2ca68005405948730686199ae
Michael Griffiths <mikey@cich.li>
2015-03-27 14:26:56 +0000

    TRDR-22: :column metadata for sets should be an int
    Signed-off-by: Nicola Mometto <brobronsa@gmail.com>

--- src/main/clojure/clojure/tools/reader.clj
+++ src/main/clojure/clojure/tools/reader.clj
@@ -353,7 +353,7 @@
   [rdr _]
   (let [[start-line start-column] (starting-line-col-info rdr)
         ;; subtract 1 from start-column so it includes the # in the leading #{
-        start-column (if start-column (dec start-column))
+        start-column (if start-column (int (dec start-column)))
         the-set (PersistentHashSet/createWithCheck (read-delimited \} rdr true))
         [end-line end-column] (ending-line-col-info rdr)]
     (with-meta the-set
